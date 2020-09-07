99553515a42d3a5991f2f87a89b888ad62a73648
lzxue <lzx199065@gmail.com>
2020-09-07 21:33:39 +0800

    chore: init

---
+++ .fatherrc.ts
@@ -0,0 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs';
+export default {
+
+  esm: 'babel',
+  cjs: 'babel',
+  umd:{
+    name: 'L7.District',
+    file: 'l7-district',
+    sourcemap: true,
+    globals: {
+      '@antv/l7': 'L7'
+    },
+  },
+  extraRollupPlugins:[
+    commonjs(
+      {
+        namedExports: {
+          eventemitter3: [ 'EventEmitter' ],
+        }
+      }
+    )
+  ],
+};
